3b630fd99db814440c28262477fcf54f9fa7471c
AYUSH KUMAR <61660372+aayush138@users.noreply.github.com>
2024-01-10 22:07:51 +0530

    app.py
    ready for render deployment

--- app.py
+++ app.py
@@ -615,5 +615,6 @@ def logout():
     return redirect(url_for("index")) 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# App Exection.
+if __name__== "__main__":
+    app.run(debug=False, host='0.0.0.0')
